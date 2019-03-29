caed50255ac6c102858e46c3e46d8c7954d74256
n0bis <madsfalken@Madss-MacBook-Pro.local>
2019-03-29 10:01:30 +0100

    base structure of project

---
+++ restAPIJwt/src/main/java/api/domain/UserDTO.java
@@ -0,0 +1,16 @@
+package api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private String name;
+
+}
