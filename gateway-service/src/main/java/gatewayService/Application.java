7cc4580223a9947eec69eb6f58823a43d9899433
n0bis <madsfalken@Madss-MacBook-Pro.local>
2019-04-12 16:22:23 +0200

    setup swagger ?

--- gateway-service/src/main/java/gatewayService/Application.java
+++ gateway-service/src/main/java/gatewayService/Application.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
--- image-service/src/main/java/imageService/Application.java
+++ image-service/src/main/java/imageService/Application.java
@@ -3,13 +3,34 @@ package imageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public Docket swaggerImageApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("imageService.controllers"))
+                .build()
+                .apiInfo(
+                        new ApiInfoBuilder()
+                        .version("1.0")
+                        .title("Image service API")
+                        .description("Image service API v1.0")
+                        .build()
+                );
+    }
 }
