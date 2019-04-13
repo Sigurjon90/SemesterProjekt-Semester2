1ad3d0cb24bafc1ba3c9c217babc18da1542ae6d
Måkl <mihen13@student.sdu.dk>
2019-04-13 10:37:36 +0200

    House keeping

--- citizensAPI/src/main/java/API/Application.java
+++ citizensAPI/src/main/java/API/Application.java
@@ -10,22 +10,10 @@ package API;
  * @author sigur
  */
 
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicLong;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
