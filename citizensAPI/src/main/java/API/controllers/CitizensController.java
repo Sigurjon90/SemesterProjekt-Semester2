e7caadce036294f9c837d4838f6bf48d113c0473
Måkl <mihen13@student.sdu.dk>
2019-04-14 10:05:59 +0200

    CrossOrigin added

--- citizensAPI/src/main/java/API/controllers/CitizensController.java
+++ citizensAPI/src/main/java/API/controllers/CitizensController.java
@@ -18,6 +18,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author sigur
  * @author thinkbuntu
  */
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/citizens")
 public class CitizensController {
