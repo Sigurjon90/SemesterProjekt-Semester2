b6ccc0e182a65bb86053ab1935fd6b204ee12a5b
n0bis <madsfalken@Madss-MacBook-Pro.local>
2019-04-17 11:40:53 +0200

    update dto we want the dates

--- Diary/src/main/java/API/entities/DiaryDTO.java
+++ Diary/src/main/java/API/entities/DiaryDTO.java
@@ -1,11 +1,11 @@
 package API.entities;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
 import java.util.UUID;
 
 @Setter
@@ -13,16 +13,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DiaryDTO {
+    
     private UUID Id;
     private String content;
     private UUID authorID;
     private UUID citizenID;
     private String title;
- //   private Date dateCreated;
-   // private Date dateModified;
-
-
-
-
+    private Date dateCreated;
+    private Date dateModified;
 
 }
