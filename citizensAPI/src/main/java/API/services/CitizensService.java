836a3925f899c30a8bc7ca515dbcb242af51580b
Mikkel <42960598+mdhenriksen@users.noreply.github.com>
2019-04-17 11:13:28 +0200

    Feature/cititzens api (#13)
    * gitignore added
    
    * DTO created, Repositores almost done
    
    * Update CitizensRepositories
    
    * some changes on Citizens - repositores
    
    * pom updated, CitizensDTO - service - repositories
    
    * Applicaiton file in progress
    
    In progress application
    
    * Application file implemented
    
    Implemented application.properties and set the connections in the repository. This way everyone can use the branch with their own credentials.
    
    * CreateCitizen in progress
    
    In progress
    
    * Create Citizen from createDTO finished
    
    * Archived status added
    
    Archived status added to Citizen and CitizenDTO. Is automatically set to false when creating a Citizen. Should be set to TRUE when deleting a Citizen
    
    * Renaming
    
    * Get Citizens working
    
    * Get all Citizens now returning DTO
    
    * FindCitizen not working
    
    * Find by ID working
    
    * Create Citizen now has dateCreated and authorID
    
    * Delete implemented
    
    * Only returning HttpStatus
    
    * Can't find Citizen if archived
    
    * Single update & batch update
    
    * deleteJournal renamed to deleteCitizen
    
    * changed repostory to use models
    
    * correctly closing the connection
    
    * fixed so we're not getting archived data out
    
    * filtered find citizen
    
    * house keeping
    
    * refactored getCitizens service
    
    * Interfaces
    
    For looses coupling yo'
    
    * Fixed SQL Array and closed connection errors
    
    * House keeping
    
    * getMyUsers implemented
    
    Needs to take a list of users UUID's assigned to the requesting user
    
    * Comments
    
    * Renaming getMyUsers to getMyCitizens
    
    * House keeping
    
    * CrossOrigin added
    
    * cors gonna be allowed throu gateway service
    
    * rename listOfId -> listOfCitizensIds
    
    * apply method to interface
    
    * rename of listOfId
    
    * directly add citizen to the list
    
    * remove unused variable
    
    * add citizen directly to list
    
    * housekeeping
    
    * added missing sql and params to citizens
    
    * renamed route
    
    * apply method to interface and house keeping
    
    * refactored modify citizen

---
+++ citizensAPI/src/main/java/API/services/CitizensService.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package API.services;
+
+import API.entities.Citizen;
+import API.entities.CitizenDTO;
+import API.entities.CreateDTO;
+import API.entities.DeleteDTO;
+import API.repositories.CitizensRepository;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author sigur
+ */
+@Service
+public class CitizensService implements ICitizensService {
+
+    @Autowired
+    private CitizensRepository citizensRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // Create Citizen from CreateDTO
+    @Override
+    public CitizenDTO createCitizen(CreateDTO createDTO) {
+        // Map to citizen
+        Citizen citizen = modelMapper.map(createDTO, Citizen.class);
+        
+        Citizen citizenCreated = citizensRepository.createCitizen(citizen);
+
+        if (citizen != null) {
+            return modelMapper.map(citizenCreated, CitizenDTO.class);
+        }
+        return null;
+    }
+
+    @Override
+    public List<CitizenDTO> getCitizens() {
+        List<Citizen> citizensList = citizensRepository.getCitizens();
+        if (citizensList == null) return null;
+        return citizensList.stream().map(citizen -> modelMapper.map(citizen, CitizenDTO.class)).collect(Collectors.toList());
+    }
+    
+    @Override
+    public List<CitizenDTO> getMyCitizens(List<UUID> listOfCitizensIds) {
+        List<Citizen> citizensList = citizensRepository.getMyCitizens(listOfCitizensIds);
+        if (citizensList == null) return null;
+        return citizensList.stream().map(citizen -> modelMapper.map(citizen, CitizenDTO.class)).collect(Collectors.toList());
+    }
+
+    @Override
+    public CitizenDTO findCitizen(UUID id) {
+        Citizen citizen = citizensRepository.findCitizen(id);
+
+        if (citizen != null) {
+            return modelMapper.map(citizen, CitizenDTO.class);
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<CitizenDTO> batchUpdate(List<CitizenDTO> citizenDTOList) {
+        List<Citizen> citizensList = citizenDTOList.stream().map(citizenDTO
+                -> modelMapper.map(citizenDTO, Citizen.class)).collect(Collectors.toList());
+
+        List<Citizen> citizenList = citizensRepository.batchUpdate(citizensList);
+
+        if (citizenList != null) {
+            return citizenList.stream().map(citizen
+                    -> modelMapper.map(citizen, CitizenDTO.class)).collect(Collectors.toList());
+        }
+        return null;
+
+    }
+
+    @Override
+    public boolean deleteCitizen(DeleteDTO deleteDTO) {
+        Citizen citizen = modelMapper.map(deleteDTO, Citizen.class);
+        return citizensRepository.deleteCitizen(citizen);
+    }
+}
