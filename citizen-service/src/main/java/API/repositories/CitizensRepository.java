6a5e0f90e252f34b20ea50a01e257ccb0372a345
Måkl <mihen13@student.sdu.dk>
2019-05-04 11:11:50 +0200

    Citizen now takes AuthorID from token

--- citizen-service/src/main/java/API/repositories/CitizensRepository.java
+++ citizen-service/src/main/java/API/repositories/CitizensRepository.java
@@ -152,9 +152,10 @@ public class CitizensRepository implements ICitizensRepository {
 
     @Override
     public boolean deleteCitizen(UUID id, UUID authorId) {
-        try (PreparedStatement deleteCitizen = connection.prepareStatement("UPDATE citizens SET archived = false, author_id = ? WHERE id = ?")) {
+        try (PreparedStatement deleteCitizen = connection.prepareStatement("UPDATE citizens SET archived = true, author_id = ? WHERE id = ?")) {
             deleteCitizen.setObject(1, authorId);
             deleteCitizen.setObject(2, id);
+           
 
             int affectedRows = deleteCitizen.executeUpdate();
 
@@ -188,15 +189,16 @@ public class CitizensRepository implements ICitizensRepository {
                 }
 
                 PreparedStatement updateCitizen = this.connection.prepareStatement("UPDATE citizens SET name = ?,"
-                        + "address = ?, city = ?, zip = ?, phone = ?, author_id = ? WHERE id = ? RETURNING id, name, address, city, zip, cpr, phone, archibed, date_created, author_id, (SELECT array(SELECT diagnose FROM diagnose WHERE diagnose.citizens_id = ?)) AS diagnoses");
+                        + "address = ?, city = ?, zip = ?, phone = ?, author_id = ? WHERE id = ? RETURNING id, name, address, city, zip, cpr, phone, archived, date_created, author_id, (SELECT array(SELECT diagnose FROM diagnose WHERE diagnose.citizens_id = ?)) AS diagnoses");
 
                 updateCitizen.setString(1, citizen.getName());
                 updateCitizen.setString(2, citizen.getAddress());
                 updateCitizen.setString(3, citizen.getCity());
                 updateCitizen.setInt(4, citizen.getZip());
                 updateCitizen.setInt(5, citizen.getPhoneNumber());
-                updateCitizen.setObject(6, citizen.getAuthorId());
+                updateCitizen.setObject(6, authorId);
                 updateCitizen.setObject(7, citizen.getId());
+                updateCitizen.setObject(8, citizen.getId());
 
                 ResultSet citizensChangedResult = updateCitizen.executeQuery();
 
