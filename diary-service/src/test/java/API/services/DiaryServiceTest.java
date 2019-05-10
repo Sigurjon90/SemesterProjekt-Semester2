782f7324bfd1a30628b239774caf4633b9ea3ffe
Jones_lund <jonas.lund@hotmail.dk>
2019-05-10 09:59:00 +0200

    lidt mere

--- diary-service/src/test/java/API/services/DiaryServiceTest.java
+++ diary-service/src/test/java/API/services/DiaryServiceTest.java
@@ -5,7 +5,11 @@
  */
 package API.services;
 
+import API.entities.Diary;
 import API.entities.DiaryDTO;
+import API.repositories.IDiaryRepository;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +19,21 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.is;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
 
 /**
  *
@@ -24,30 +43,57 @@ public class DiaryServiceTest {
 
     public DiaryServiceTest() {
     }
+    @InjectMocks
+    private DiaryService diaryService;
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
+    @Mock
+    private IDiaryRepository repositoryMock;
 
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    @Mock
+    private ModelMapper modelMapper;
+
+    private UUID citizensId = UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3de");
+    private UUID authorIDDiary = UUID.fromString("6a7a8cb3-6502-4acc-b302-72b9e30bdf8e");
+    private UUID diaryId = UUID.fromString("e2a96b4d-edff-497b-a094-268e3b86ed28");
+    private Date date = new Date();
+    List<UUID> listOfCitizenIds = Arrays.asList(UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3de"), UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3da"));
+    
+    private final List<Diary> diary = Arrays.asList(new Diary(UUID.fromString("06d0166d-46b6-4bb5-8572-9299fc87c3dc"), "abba", UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3dd"), UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3de"), new Date(System.currentTimeMillis()), null, "titel44"));
+
+    private final List<Diary> diaries = Arrays.asList(new Diary(UUID.fromString("06d0166d-46b6-4bb5-8572-9299fc87c3dc"), "abba", UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3dd"), UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3gg"), new Date(System.currentTimeMillis()), null, "titel22"),
+            new Diary(UUID.fromString("06d0166d-46b6-4bb5-8572-9299fc87c3qq"), "walla", UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3dd"), UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3ff"), new Date(System.currentTimeMillis()), null, "titel33"));
+
+    private final List<DiaryDTO> diaryDTO = java.util.Arrays.asList(
+            new DiaryDTO(UUID.fromString("06d0166d-46b6-4bb5-8572-9299fc87c3dc"), "abba", UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3dd"), UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3de"), "titel", new Date(System.currentTimeMillis()), null),
+            new DiaryDTO(UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3df"), "lmao", UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3dc"), UUID.fromString("06d0166d-56b6-4bb5-8572-9299fc87c3da"), "titel1", new Date(System.currentTimeMillis()), null)
+    );
+
+    DiaryDTO createDTO = new DiaryDTO(UUID.fromString("e7c41f9e-cace-4f4d-845f-f3c4ed3fdaf9"), "Halló", authorIDDiary, citizensId, "titill", new Date(), null);
+
+    Diary createDiaryTest = new Diary(diaryId, "hallo", authorIDDiary, citizensId, this.date, null, "titil");
 
     @Before
     public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
+        MockitoAnnotations.initMocks(this);
     }
 
     /**
      * Test of createDiary method, of class DiaryService.
+     *
+     * @throws java.lang.Exception
      */
     @Test
-    public void testCreateDiary() {
+    public void testCreateDiary() throws Exception {
+        when(repositoryMock.createDiary(createDiaryTest)).thenReturn(Optional.of(diary.get(0)));
+        when(modelMapper.map(diary.get(0), Diary.class)).thenReturn(diary.get(0));
+        when(modelMapper.map(createDiaryTest, Diary.class)).thenReturn(diary.get(0));
 
-        fail("The test case is a prototype.");
+        DiaryDTO actual = diaryService.createDiary(Optional.of(createDTO));
+
+        verify(repositoryMock, times(1)).createDiary(diary.get(0));
+        verifyNoMoreInteractions(repositoryMock);
+
+        assertThat(actual, is(diaryDTO.get(0)));
     }
 
     /**
@@ -74,7 +120,17 @@ public class DiaryServiceTest {
     @Test
     public void testGetDiaries() {
 
-        fail("The test case is a prototype.");
+        when(repositoryMock.getDiaries(listOfCitizenIds)).thenReturn(diaries);
+        when(modelMapper.map(diary.get(0), DiaryDTO.class)).thenReturn(diaryDTO.get(0));
+
+        List<DiaryDTO> actual = diaryService.getDiaries(listOfCitizenIds);
+
+        verify(repositoryMock, times(1)).getDiaries(listOfCitizenIds);
+        verifyNoMoreInteractions(repositoryMock);
+
+        modelMapper.validate();
+
+        assertThat(actual, is(Arrays.asList(diaryDTO.get(0))));
     }
 
     /**
