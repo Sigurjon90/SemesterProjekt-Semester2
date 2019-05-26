ccb5df019d8bed4e6dc3ad08151d2f8d2288fab9
n0bis <supermads@hotmail.com>
2019-05-26 08:49:13 +0200

    Bugfix/builds (#43)
    * Update build.command
    
    * Update WindowsBuild.bat
    
    * Update WindowsBuild.bat

--- WindowsBuild.bat
+++ WindowsBuild.bat
@@ -1,12 +1,13 @@
 @ECHO OFF
-
+start cmd /k "cd %~dp0common-service && mvn package && mvn install:install-file -Dfile=target/common-service-1.0-SNAPSHOT.jar -DpomFile=pom.xml"
 start cmd /k "cd %~dp0eureka-server && mvn spring-boot:run"
-timeout 10
+timeout 20
 start cmd /k "cd %~dp0gateway-service && mvn spring-boot:run"
 start cmd /k "cd %~dp0diary-service && mvn spring-boot:run"
 start cmd /k "cd %~dp0auth-service && mvn spring-boot:run"
 start cmd /k "cd %~dp0citizen-service && mvn spring-boot:run"
 start cmd /k "cd %~dp0journal-service && mvn spring-boot:run"
 start cmd /k "cd %~dp0user-service && mvn spring-boot:run"
+start cmd /k "cd %~dp0frontend && npm install && npm start"
 
 PAUSE
